207bb14d25630013b03252158e54f4087b13b0ee
Alexey <algoritm211@gmail.com>
2022-10-10 00:25:57 +0300

    feat: add angular-material and basic application structure

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './system/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './system/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/frontend/src/app/system/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maorix-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {
+  constructor() {}
+}
