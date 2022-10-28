d4473af618213b81cd1687e612df2d9065d108f3
Alexey <algoritm211@gmail.com>
2022-10-29 01:54:33 +0300

    feat: add qr-code scanner

--- apps/frontend/src/app/system/pages/contribution-scanner/barcode-scanner/barcode-scanner.component.ts
+++ apps/frontend/src/app/system/pages/contribution-scanner/barcode-scanner/barcode-scanner.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgxScannerQrcodeComponent} from "ngx-scanner-qrcode";
 
 @Component({
   selector: 'maorix-barcode-scanner',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./barcode-scanner.component.css'],
 })
 export class BarcodeScannerComponent {
-  constructor() {}
+  @ViewChild('action', { static: true }) scanner: NgxScannerQrcodeComponent;
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  handleData($event: string) {
+    if ($event) {
+      this.scanner.stop().complete()
+      void this.router.navigate(['/dashboard'])
+    }
+  }
+
+  onError($event: ErrorEvent) {
+    if ($event.message === 'Permission denied') {
+      alert('Please check your camera permissions')
+    }
+  }
 }
--- apps/frontend/src/app/system/pages/contribution-scanner/contribution-scanner.module.ts
+++ apps/frontend/src/app/system/pages/contribution-scanner/contribution-scanner.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ContributionScannerRoutingModule } from './contribution-scanner-routing.module';
-import { ContributionScannerComponent } from './contribution-scanner.component';
+import {ContributionScannerRoutingModule} from './contribution-scanner-routing.module';
+import {ContributionScannerComponent} from './contribution-scanner.component';
 import {BarcodeScannerComponent} from "./barcode-scanner/barcode-scanner.component";
+import {NgxScannerQrcodeModule} from 'ngx-scanner-qrcode';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+
 
 @NgModule({
-  declarations: [ContributionScannerComponent, BarcodeScannerComponent],
-  imports: [CommonModule, ContributionScannerRoutingModule],
+  declarations: [
+    ContributionScannerComponent,
+    BarcodeScannerComponent
+  ],
+  imports: [
+    CommonModule,
+    ContributionScannerRoutingModule,
+    NgxScannerQrcodeModule,
+    MatButtonModule,
+    MatIconModule
+  ],
 })
-export class ContributionScannerModule {}
+export class ContributionScannerModule {
+}
